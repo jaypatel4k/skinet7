62f3408fc9ec8d905068f21dd785a84518c5729c
jaypatel4k <jaypatel4k@gmail.com>
2024-09-29 16:41:51 +0530

    End of Section 10: Angular Routing
    End of Section 10: Angular Routing

---
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ShopService } from '../../../core/services/shop.service';
+import { Product } from '../../../models/product';
+import { CurrencyPipe } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatDivider } from '@angular/material/divider';
+import { MatInput } from '@angular/material/input';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: true,
+  imports: [
+    CurrencyPipe,
+    MatIcon,
+    MatFormField,
+    MatDivider,
+    MatLabel,
+    MatInput
+  ],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.scss'
+})
+export class ProductDetailsComponent implements OnInit {
+  private shopService = inject(ShopService);
+  private activatedRoute = inject(ActivatedRoute);
+  
+  product?: Product;
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+  loadProduct() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!id) return;
+    this.shopService.getProduct(+id).subscribe({
+      next: product => {
+        this.product = product;
+      },
+      error: error => console.log(error)
+    })
+  }
+}
