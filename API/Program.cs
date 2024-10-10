66d3f51db660ea2ec91a44e21cfc83df1f429d06
jaypatel4k <jaypatel4k@gmail.com>
2024-10-10 18:11:38 +0530

    End Of Section14: API - Identity
    End Of Section14: API - Identity

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Services;
@@ -28,6 +29,11 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
     return ConnectionMultiplexer.Connect(configuration);
 });
 builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<StoreContext>();
+
+
 
 var app = builder.Build();
 
@@ -47,6 +53,8 @@ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
     .WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
+//app.MapIdentityApi<AppUser>();
+app.MapGroup("api").MapIdentityApi<AppUser>(); // api/login or api/register
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,17 +1,15 @@
 using System.Reflection;
-using System.Security.Cryptography;
 using Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class StoreContext(DbContextOptions options) : DbContext(options)
+    // public class StoreContext(DbContextOptions options) : DbContext(options)
+    public class StoreContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
     {
         public DbSet<Product> Products{get;set;}
-
-        // public DbSet<ProductBrand> ProductBrands{get;set;}
-
-        // public DbSet<ProductType> ProductTypes{get;set;}
+        public DbSet<Address> Addresses{get;set;}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
